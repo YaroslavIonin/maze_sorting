0304b2e52086d885927dfc2cabd172f1cdced713
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-10-26 23:13:16 +0500

    update constants

--- constants.py
+++ constants.py
@@ -1,4 +1,15 @@
+from models import State
+
 ROOM_POS = (2, 4, 6, 8)
 STOP_POS = (0, 1, 3, 5, 7, 9, 10)
 COST = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
 TARGET_ROOM = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
+TARGET_STATE = State(
+    hallway=('.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', ),
+    rooms=(('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'),),
+)
+
+# TARGET_STATE = State(
+#     hallway=('.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', ),
+#     rooms=(('C', 'A'), ('B', 'B'), ('D', 'C'), ('D', 'A')),
+# )

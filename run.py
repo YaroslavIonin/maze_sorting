1dad30008a969b1acd25260b1cc44e68f2a50523
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-10-26 23:20:01 +0500

    update func solve using dijkstra

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import sys
+import itertools
+
+from heapq import heappush, heappop
 
 from models import State
+from moves import get_moves
+from constants import TARGET_STATE
 from utils import parse_input_lines
 
 
@@ -15,9 +20,31 @@ def solve(lines: list[str]) -> int:
         минимальная энергия для достижения целевой конфигурации
     """
 
-    state: State = parse_input_lines(lines)
+    base_state: State = parse_input_lines(lines)
 
-    return state
+    counter = itertools.count()
+    heap = [(0, next(counter), base_state)]
+
+    best_moves = {
+        base_state: 0,
+    }
+
+    while heap:
+        cost, _, state = heappop(heap)
+        if state == TARGET_STATE:
+            return cost
+
+        if cost > best_moves[state]:
+            continue
+
+        moves = get_moves(state)
+        for new_state, extra_cost in moves:
+            new_cost = cost + extra_cost
+            if new_state not in best_moves or new_cost < best_moves[new_state]:
+                best_moves[new_state] = new_cost
+                heappush(heap, (new_cost, next(counter), new_state))
+
+    return None
 
 
 def main():

3d90a4919dfcdc492302120da5af6dc90da12c1c
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-10-26 23:07:54 +0500

    update typing in State

--- models/state.py
+++ models/state.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class State:
-    hallway: tuple[str]
-    rooms: tuple[tuple[str]]
+    hallway: tuple[str, ...]
+    rooms: tuple[tuple[str, str], ...]
